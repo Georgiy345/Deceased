67db1018970563ec636aa0be3f5b3dd7bd6e41e8
Georgiy <gegorgan@gmail.com>
2024-07-02 23:18:26 +0400

    0.1.1

---
+++ deceased/src/main/java/com/practice/deceased/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.practice.deceased.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainpage(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "mainpage";
+    }
+}
